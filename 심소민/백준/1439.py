119f4a04473cf43ab1b64a3521822f822be0bccb
somin <shimsomin20@gmail.com>
2024-10-04 23:03:02 +0900

    Feat: 백준 11403, 1439, 9935

---
+++ 심소민/백준/1439.py
@@ -0,0 +1,17 @@
+"""
+* Author    : SHIM SOMIN
+* Date      : 2024.09.30 (Mon)
+* Runtime   : 44 ms 
+* Memory    : 33240 KB  
+* Algorithm : 그리디
+"""
+
+import math
+
+s = input()
+cnt = 0
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        cnt += 1
+
+print(math.ceil(cnt/2))
