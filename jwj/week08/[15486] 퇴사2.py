1b0d5fbe13595c75029e4920b875b38bf0c51fc0
jwj <dnwls16071@naver.com>
2024-11-21 20:10:01 +0900

    Feat: 15486번 코드 추가

---
+++ jwj/week08/[15486] 퇴사2.py
@@ -0,0 +1,30 @@
+'''
+* Author    : Jang Woo Jin
+* Date      : 2024.11.21(Thr)
+* Runtime   : 2540 ms
+* Memory    : 168760 KB 
+* Algorithm : Dynamic Programming
+'''
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+T = [0 for i in range(N+1)]
+P = [0 for i in range(N+1)]
+dp = [0 for i in range(N+1)]
+
+for i in range(1, N+1):
+    t, p = map(int, input().split())
+    T[i] = t
+    P[i] = p
+
+for i in range(1, N+1):
+    dp[i] = max(dp[i], dp[i-1])
+    deadline = i + T[i] - 1
+    if deadline <= N:
+        dp[deadline] = max(dp[deadline], dp[i - 1] + P[i])
+print(max(dp))
+
+    
